a7e3d92d086231bed20b988dce95d3690a0dc4a3
Pankaja Shankar <pankaja_us@yahoo.com>
2026-04-09 13:34:53 -0400

    podcast generator shell script

---
+++ podcast_gen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# below are already there in the git action workflow file
+# pip install --upgrade pip
+# pip install -r requirements.txt
+
+# podcast feed generator processing main kick-off
+python feed.py
